d687c05608ef2e8b5d8cd0fc7d6a5c5356fcda53
Vinchenso <chensok@gmail.com>
2017-02-19 15:50:35 +0200

    install vue, webpack-rails

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -27,6 +27,33 @@ var config = {
 
     filename: production ? '[name]-[chunkhash].js' : '[name].js'
   },
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+          }
+          // other vue-loader options go here
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'stage-2']
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.common.js'
+    }
+  },
+  devtool: '#eval-source-map',
 
   resolve: {
     root: path.join(__dirname, '..', 'webpack')
@@ -45,23 +72,23 @@ var config = {
 };
 
 if (production) {
-  config.plugins.push(
-    new webpack.NoErrorsPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: { warnings: false },
-      sourceMap: false
-    }),
-    new webpack.DefinePlugin({
-      'process.env': { NODE_ENV: JSON.stringify('production') }
-    }),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin()
-  );
+config.plugins.push(
+  new webpack.NoErrorsPlugin(),
+  new webpack.optimize.UglifyJsPlugin({
+    compressor: { warnings: false },
+    sourceMap: false
+  }),
+  new webpack.DefinePlugin({
+    'process.env': { NODE_ENV: JSON.stringify('production') }
+  }),
+  new webpack.optimize.DedupePlugin(),
+  new webpack.optimize.OccurenceOrderPlugin()
+);
 } else {
-  config.devServer = {
-    port: devServerPort,
-    headers: { 'Access-Control-Allow-Origin': '*' }
-  };
+config.devServer = {
+  port: devServerPort,
+  headers: { 'Access-Control-Allow-Origin': '*' }
+};
   config.output.publicPath = '//localhost:' + devServerPort + '/webpack/';
   // Source maps
   config.devtool = 'cheap-module-eval-source-map';
--- webpack/application.js
+++ webpack/application.js
@@ -1,1 +1,10 @@
-console.log("Hello world!");
+import Vue from 'vue'
+import App from './App.vue'
+
+$(document).ready(function(){
+  new Vue({
+    el: '#app',
+    render: h => h(App)
+
+  })
+})
